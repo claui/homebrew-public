a6917de49f7c08049a2ec01051c0fff4055e607e
Claudia <claui@users.noreply.github.com>
2018-02-17 11:58:17 +0100

    znapzupport 0.0.4

--- Formula/znapzupport.rb
+++ Formula/znapzupport.rb
@@ -1,22 +1,26 @@
 class Znapzupport < Formula
   desc "Collection of CLI helpers for ZnapZend"
   homepage "https://github.com/claui/znapzupport"
-  url "https://github.com/claui/znapzupport/archive/v0.0.3.tar.gz"
-  sha256 "f800c63eff74dacffec4e9ed0c14e9b1d90c19b084cc33d919425e32c4fc63a8"
+  url "https://github.com/claui/znapzupport/archive/v0.0.4.tar.gz"
+  sha256 "49e4b91f721154b7c4cfe364143645824eaceea2148f8b9101b66900e3747653"
 
   bottle :unneeded
 
   def install
-    bin_name = "znaphodl"
-    (bin/bin_name).write <<~EOS
-      #!/bin/bash
-      exec "#{libexec/bin_name}" "$@"
-    EOS
+    bin_names = %w[znaphodl znaphodlz znaplizt zpoolz]
+    bin_names.each do |bin_name|
+      (bin/bin_name).write <<~EOS
+        #!/bin/bash
+        exec "#{libexec/bin_name}" "$@"
+      EOS
 
-    libexec.install [
-      "bin/#{bin_name}",
-      "libexec/#{bin_name}.bash",
-    ]
+      libexec.install [
+        "bin/#{bin_name}",
+        "libexec/#{bin_name}.bash",
+      ]
+    end
+
+    libexec.install "libexec/dataset_id.bash"
 
     doc.install "LICENSE.md", "README.md"
   end
