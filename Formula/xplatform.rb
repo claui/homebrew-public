3521d30b7946e50308ba708b4f3915ee19352e60
Claudia <claui@users.noreply.github.com>
2018-02-19 15:21:41 +0100

    xplatform 0.0.10

--- Formula/xplatform.rb
+++ Formula/xplatform.rb
@@ -1,28 +1,40 @@
 class Xplatform < Formula
   desc "Platform-independent aliases for GNU utilities"
   homepage "https://bitbucket.org/clauii/xplatform"
-  url "https://bitbucket.org/clauii/xplatform/get/v0.0.9.tar.gz"
-  sha256 "17ffaa84c5fff078c7be12b79174e36fea02add875456b8f0065ac1fd42ae130"
+  url "https://bitbucket.org/clauii/xplatform/get/v0.0.10.tar.gz"
+  sha256 "6a5535b4e242d61d3087f7cdd1d912c2660a018ccd5bfc78a1f5b3a7f4c89f02"
   head "https://bitbucket.org/clauii/xplatform.git"
 
   bottle :unneeded
 
-  depends_on "coreutils"
-  depends_on "findutils"
-  depends_on "gcal"
-  depends_on "gnu-getopt"
-  depends_on "gnu-sed"
-  depends_on "gpatch"
+  depends_on "coreutils" => :recommended
+  depends_on "findutils" => :recommended
+  depends_on "gawk" => :optional # May shadow /usr/bin/awk
+  depends_on "gcal" => :recommended
+  depends_on "gnu-getopt" => :recommended
+  depends_on "gnu-sed" => :recommended
+  depends_on "gpatch" => :optional # May shadow /usr/bin/patch
+  depends_on "grep" => :recommended
 
   def install
-    bin_names = %w[mcal platform_id]
-    bin_names.concat xplatform_filenames_for("coreutils")
-    bin_names.concat xplatform_filenames_for("findutils")
-    bin_names.concat xplatform_filenames_for("gcal",
-      :exclude => ["gcal2txt"])
-    bin_names.concat %w[xgetopt] # gnu-getopt
-    bin_names.concat xplatform_filenames_for("gnu-sed")
-    bin_names.concat %w[xpatch] # gpatch
+    bin_names = [
+      "coreutils",
+      "findutils",
+      ["gawk", :exclude => [/gawk-/]],
+      ["gcal", :include => ["mcal"], :exclude => ["gcal2txt"]],
+      ["gnu-getopt", :name_prefix => ""],
+      "gnu-sed",
+      ["gpatch", :name_prefix => ""],
+      "grep",
+    ].flat_map do |package_name, options = {}|
+      if build.with? package_name
+        xplatform_filenames_for(package_name, options)
+      else
+        []
+      end
+    end
+
+    bin_names << 'platform_id'
 
     bin_names.each do |bin_name|
       (bin/bin_name).write <<~EOS
@@ -47,40 +59,66 @@ class Xplatform < Formula
 
   def xplatform_filenames_for(upstream_formula, opt = {})
     dir = Formula[upstream_formula].opt_bin
+    name_prefix = opt.fetch(:name_prefix, "g")
+    include_list = opt.fetch(:include, [])
     exclude_list = opt.fetch(:exclude, [])
-    filenames = []
-    dir.find do |path|
+    filenames = include_list + dir.find.map do |path|
       next if path.directory?
       bin_name = path.basename.to_s
-      next if exclude_list.include?(bin_name)
-      bin_name.scan(/^g(.*)/) do |raw_name|
-        filenames << "x#{raw_name[0]}"
-      end
+      next if exclude_list.any? { |x| x === bin_name }
+      /^#{name_prefix}(.*)/.match(bin_name) { "x#{$1}" }
     end
-    filenames.sort
+    filenames.compact
   end
 
   test do
-    # coreutils
-    (testpath/"test").write("test")
-    (testpath/"test.sha1").write("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3 test")
-    system bin/"xsha1sum", "-c", "test.sha1"
-    system bin/"xln", "-f", "test", "test.sha1"
+    if build.with? "coreutils"
+      (testpath/"test").write("test")
+      (testpath/"test.sha1").write("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3 test")
+      system bin/"xsha1sum", "-c", "test.sha1"
+      system bin/"xln", "-f", "test", "test.sha1"
+    end
 
-    # findutils
-    touch "HOMEBREW"
-    assert_match "HOMEBREW", shell_output("#{bin}/xfind .")
+    if build.with? "findutils"
+      touch "HOMEBREW"
+      assert_match "HOMEBREW", shell_output("#{bin}/xfind .")
+    end
 
-    # gcal
-    date = shell_output("#{bin}/xdate +%Y")
-    assert_match(date, shell_output(bin/"xcal"))
+    if build.with? "gawk"
+      assert_match /Foo/, shell_output("#{bin}/xawk 1 <<< Foo")
+    end
 
-    # gnu-getopt
-    system bin/"xgetopt", "-o", "--test"
+    if build.with? "gcal"
+      date = shell_output("#{bin}/xdate +%Y")
+      assert_match(date, shell_output(bin/"xcal"))
+    end
 
-    # gnu-sed
-    (testpath/"test.txt").write "Hello world!"
-    system bin/"xsed", "-i", "s/world/World/g", "test.txt"
-    assert_match /Hello World!/, File.read("test.txt")
+    if build.with? "gnu-getopt"
+      system bin/"xgetopt", "-o", "--test"
+    end
+
+    if build.with? "gnu-sed"
+      (testpath/"test.txt").write "Hello world!"
+      system bin/"xsed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
+    end
+
+    if build.with? "gpatch"
+      (testpath/"test.c").write "#define FOO\n"
+      (testpath/"patch").write <<~EOS
+        1c1
+        < #define FOO
+        ---
+        > #define BAR
+      EOS
+      system bin/"xpatch", testpath/"test.c", testpath/"patch"
+      assert_match '#define BAR', File.read(testpath/"test.c")
+    end
+
+    if build.with? "grep"
+      system "#{bin}/xgrep Cafifornia /usr/include/stdlib.h; [ $? -eq 1 ]"
+      assert_match /California, Berkeley and its contributors.$/,
+        shell_output("#{bin}/xgrep California /usr/include/stdlib.h")
+    end
   end
 end
