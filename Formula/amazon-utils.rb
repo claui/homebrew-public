6952625a5b7baaf7f118e622d4404bce984f83be
Claudia <claui@users.noreply.github.com>
2018-06-07 11:53:41 +0200

    amazon-utils 0.0.7

--- Formula/amazon-utils.rb
+++ Formula/amazon-utils.rb
@@ -1,8 +1,8 @@
 class AmazonUtils < Formula
   desc "Collection of CLI tools to improve UX on Amazon’s retail websites"
   homepage "https://github.com/claui/amazon-utils"
-  url "https://github.com/claui/amazon-utils/archive/v0.0.6.tar.gz"
-  sha256 "4b42000067ddc1230135514571d6693ea0dac6247e9c3514da71b032fe16d70d"
+  url "https://github.com/claui/amazon-utils/archive/v0.0.7.tar.gz"
+  sha256 "55aba4e5f9d1bd0f4b74f0248cb3e50a8c1fec9dcfd1ae86e8d7573ecf723fec"
 
   bottle :unneeded
 
