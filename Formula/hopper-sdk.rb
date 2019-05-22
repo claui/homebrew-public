0e596f1aecdc38130e34eb8978820f128120d693
Claudia <claui@users.noreply.github.com>
2019-05-22 15:14:36 +0200

    Add hopper-sdk 4.5.11

---
+++ Formula/hopper-sdk.rb
@@ -0,0 +1,19 @@
+class HopperSdk < Formula
+  desc "Hopper SDK"
+  homepage "https://www.hopperapp.com/download/"
+  url "https://d2ap6ypl1xbe4k.cloudfront.net/HopperSDK-4.5.11.zip"
+  sha256 "10f3c5fd44038245042a350308774d57b2b53f7485ecdc2139900ee91d8aa86c"
+
+  bottle :unneeded
+
+  def install
+    include.install "include/Hopper"
+    (pkgshare/"examples").install Dir["Samples/*"]
+    doc.install "SDK Documentation.pdf"
+
+    (bin/"hopper-sdk").write <<~EOS
+      #!/bin/bash
+      exec open "#{doc}/SDK Documentation.pdf"
+    EOS
+  end
+end
