4529d4bc6781dd1f612db846d9c92894cd0336ed
Claudia <claui@users.noreply.github.com>
2020-12-06 18:57:15 +0100

    ips-util: fix license

--- Formula/ips-util.rb
+++ Formula/ips-util.rb
@@ -5,7 +5,7 @@ class IpsUtil < Formula
   homepage "https://github.com/nleseul/ips_util"
   url "https://files.pythonhosted.org/packages/e3/68/4250a1ca49778ec7bd00ac408c0cf8a56d09ab8adbfa59f63f14c84d089e/ips_util-1.0.tar.gz"
   sha256 "478502f573f2b92f6409ae9efb129025b9e2cd2d71b2c9b6f1fd1796cb311933"
-  license "NOASSERTION"
+  license "Unlicense"
 
   depends_on "python@3.9"
 
