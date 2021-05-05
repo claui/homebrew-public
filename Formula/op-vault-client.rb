95080adbd6337922b5b723cef9560c5b5a848b69
Claudia <claui@users.noreply.github.com>
2021-05-05 21:41:11 +0200

    op-vault-client 1.0.1

--- Formula/op-vault-client.rb
+++ Formula/op-vault-client.rb
@@ -20,7 +20,7 @@ class OpVaultClient < Formula
   desc "Password client script to integrate Ansible Vault with 1Password"
   homepage "https://github.com/claui"
   url "file:///dev/null"
-  version "1.0"
+  version "1.0.1"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on OnePasswordCliRequirement
@@ -29,7 +29,7 @@ class OpVaultClient < Formula
     (bin/"op-vault-client").write <<~EOS
       #!/bin/bash
       if [ "${SKIP_VAULT_PASSWORD:-0}" -ne '0' ]; then
-        printf '\0'
+        printf '\\0'
         exit 0
       fi
       op get item owerpe2tcbemjou3wyauz3bd5y --fields password
