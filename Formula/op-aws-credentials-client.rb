53d2540d7d36e0b7c0a8d4d98145c26a1bf2ec1e
Claudia <claui@users.noreply.github.com>
2021-06-05 16:09:50 +0200

    op-aws-credentials-client 1.0.1

--- Formula/op-aws-credentials-client.rb
+++ Formula/op-aws-credentials-client.rb
@@ -4,27 +4,46 @@ class OpAwsCredentialsClient < Formula
   desc "Password client script to fetch AWS credentials from 1Password"
   homepage "https://github.com/claui"
   url "file:///dev/null"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on OnePasswordCliRequirement
 
   def install
     (bin/"op-aws-credentials-client").write <<~EOS
-      #!/bin/bash
+      #!/bin/bash -eu
+      set -o pipefail
+
       if [ "${SKIP_AWS_SECRET:-0}" -ne '0' ]; then
         printf '\\0'
         exit 0
       fi
+
+      if [ "${#@}" -lt 4 ]; then
+        printf >&2 '%s:\\n\\t%s %s %s %s %s\\n' \\
+          'Usage' "$(basename -- "$0")" \\
+          '[OP_VAULT]' '[OP_ITEM]' \\
+          '[ACCESS_KEY_ID_FIELDNAME]' '[SECRET_ACCESS_KEY_FIELDNAME]' \\
+          'Example' "$(basename -- "$0")" \\
+          'ha4b0y7mdawmlpp2ywsdke57f7' 'nd9a4myjpgsqdbx4bakqb6aczw' \\
+          "'Access Key ID'" "'Secret Access Key'"
+        exit 1
+      fi
+
+      OP_VAULT="${1?}"
+      OP_ITEM="${2?}"
+      ACCESS_KEY_ID_FIELDNAME="${3?}"
+      SECRET_ACCESS_KEY_FIELDNAME="${4?}"
+
       /usr/local/bin/op get item \\
-        --fields='Access Key ID,Secret Access Key' \\
-        --vault=ekwylbfcpyh3qlpz6ba3dm7e5u \\
-        724iu2bgabeungn5d7q4obkche \\
+        --fields="${ACCESS_KEY_ID_FIELDNAME},${SECRET_ACCESS_KEY_FIELDNAME}" \\
+        --vault="${OP_VAULT}" \\
+        "${OP_ITEM}" \\
         | /usr/local/bin/jq "$(/bin/cat << EOF
           {
             Version: 1,
-            AccessKeyId: ."Access Key ID",
-            SecretAccessKey: ."Secret Access Key",
+            AccessKeyId: ."${ACCESS_KEY_ID_FIELDNAME}",
+            SecretAccessKey: ."${SECRET_ACCESS_KEY_FIELDNAME}",
           }
       EOF
           )"
@@ -33,6 +52,6 @@ class OpAwsCredentialsClient < Formula
 
   test do
     assert_match(/The account details you entered aren't saved on this device/,
-      shell_output("#{bin}/op-aws-credentials-client"))
+      shell_output("#{bin}/op-aws-credentials-client foo bar baz qux"))
   end
 end
