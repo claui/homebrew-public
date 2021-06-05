36df9eef2056b4639f858e9b952552ff6b8003bb
Claudia <claui@users.noreply.github.com>
2021-06-05 16:38:28 +0200

    op-aws-credentials-client 1.0.3

--- Formula/op-aws-credentials-client.rb
+++ Formula/op-aws-credentials-client.rb
@@ -4,7 +4,7 @@ class OpAwsCredentialsClient < Formula
   desc "Password client script to fetch AWS credentials from 1Password"
   homepage "https://github.com/claui"
   url "file:///dev/null"
-  version "1.0.2"
+  version "1.0.3"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "jq"
@@ -13,22 +13,22 @@ class OpAwsCredentialsClient < Formula
   def install
     (bin/"op-aws-credentials-client").write <<~EOS
       #!/bin/bash -eu
-      set -o pipefail
+      \\set -o pipefail
 
-      if [ "${SKIP_AWS_SECRET:-0}" -ne '0' ]; then
-        printf '\\0'
-        exit 0
+      if \\[ "${SKIP_AWS_SECRET:-0}" -ne '0' ]; then
+        \\printf '\\0'
+        \\exit 0
       fi
 
-      if [ "${#@}" -lt 4 ]; then
-        printf >&2 '%s:\\n\\t%s %s %s %s %s\\n' \\
-          'Usage' "$(basename -- "$0")" \\
+      if \\[ "${#@}" -lt 4 ]; then
+        \\printf >&2 '%s:\\n\\t%s %s %s %s %s\\n' \\
+          'Usage' "$(/usr/bin/basename -- "$0")" \\
           '[OP_VAULT]' '[OP_ITEM]' \\
           '[ACCESS_KEY_ID_FIELDNAME]' '[SECRET_ACCESS_KEY_FIELDNAME]' \\
-          'Example' "$(basename -- "$0")" \\
+          'Example' "$(/usr/bin/basename -- "$0")" \\
           'ha4b0y7mdawmlpp2ywsdke57f7' 'nd9a4myjpgsqdbx4bakqb6aczw' \\
           "'Access Key ID'" "'Secret Access Key'"
-        exit 1
+          \\exit 1
       fi
 
       OP_VAULT="${1?}"
